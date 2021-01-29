11e9ee4021ce9d948bbb87a0d3795fb55e1f6015
josejose3103 <josejose3103@penguin>
2021-01-29 17:09:48 +0900

    add rate.py

---
+++ rate.py
@@ -0,0 +1,8 @@
+import requests
+import json
+
+endPoint = 'https://api.coin.z.com/public'
+path     = '/v1/ticker?symbol=BTC'
+
+response = requests.get(endPoint + path)
+print(json.dumps(response.json(), indent=2))
