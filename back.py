d0b617bebd569339fcf43bdc2b8871f7000ffde8
josejose3103 <61366752+josejose3103@users.noreply.github.com>
2021-02-06 18:41:52 +0900

    Add files via upload

---
+++ back.py
@@ -0,0 +1,19 @@
+import datetime
+import pandas as pd
+from backtesting import Strategy
+from backtesting import Backtest
+ 
+class myCustomStrategy(Strategy):
+    def init(self):
+        pass
+ 
+    def next(self): 
+        self.buy if self.data.Close[-1]> self.data.Open[-1] else self.sell()
+ 
+df = pd.read_csv("1330.csv", skiprows=2000, names=("Datetime", "Open", "High", "Low", "Close", "Volume"), index_col='Datetime')
+# 欠損値を埋める
+df = df.interpolate()
+df.index = pd.to_datetime(df.index)
+bt = Backtest(df, myCustomStrategy, cash=100000, commission=.001)
+bt.run()
+# bt.plot()
